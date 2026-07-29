0d74c247ca08c1aabc6a5cb3ad6d5ac7af89848a
Ahmad Alaqra <52831910+alaqrasenpai@users.noreply.github.com>
2026-07-29 17:42:07 +0300

    Fix: Add Job Titles to sidebar navigation

--- frontend/app/utils/navConfig.ts
+++ frontend/app/utils/navConfig.ts
@@ -19,6 +19,12 @@ export const navConfig = [
     requiredPermission: 'hr:employees:read'
   },
   {
+    name: 'jobs.title',
+    path: '/apps/hr/jobs',
+    icon: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>',
+    requiredPermission: 'hr:employees:manage'
+  },
+  {
     name: 'sidebar.shifts',
     path: '/apps/hr/shifts',
     icon: '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>',
